f8b256d1011340c70f61ff810c3d97a8630ab862
caw <christopherwood07@gmail.com>
2013-10-25 16:48:40 -0700

    moving stuff around

--- src/analysis/code.py
+++
@@ -1,8 +0,0 @@
-# File: code.py
-# Author: Christopher Wood
-# Description: Code for a linear code (convenience stuff)
-
-import pickle
-
-class GFElem:
-	def __init__(self, coefficients, binString = None, id = -1, base = -1, exp = -1): 
